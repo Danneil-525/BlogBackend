e8b52c772836182d2a2da4f4fbe54110e9cca7fb
Xiongl09 <Xiongl09@mingyuanyun.com>
2021-08-10 18:11:51 +0800

    feat: 实现跳转对应详情页

--- app/controller/home.js
+++ app/controller/home.js
@@ -19,7 +19,27 @@ class HomeController extends Controller {
       FROM article LEFT JOIN type ON article.type_id = type.id
       `;
     const res = await this.app.mysql.query(sql);
-    ctx.body = res;
+    console.log(res);
+    ctx.body = { data: res };
+  }
+
+  async getArticleById() {
+    const { ctx } = this;
+    const id = this.ctx.params.id;
+    const sql = `
+    SELECT article.id,
+      title,
+      introduce,
+      article_content,
+      addTime,
+      view_count,
+      typeName,
+      type.id as typeId,
+      FROM article LEFT JOIN type ON article.type_id = type.id
+      WHERE article.id=${id}
+    `;
+    const res = await this.app.mysql.query(sql);
+    ctx.body = { data: res };
   }
 }
 
